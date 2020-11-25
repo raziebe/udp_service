37fccda5095a1f0f3e6d88200ba4b7d6e3bbba32
raz <raz@hiskysat.com>
2020-11-25 18:50:55 +0200

    UDS to udp

--- service_check.c
+++ service_check.c
@@ -21,11 +21,15 @@
 
 int service_disabled;
 static int listensock;
-static int termsock;
 
 #define TERM_APP_SERVICE_DISABLE 0x010
 #define TERM_APP_SERVICE_ENABLE 0x011
 #define TERM_APP_SERVICE_STATS	0x18
+
+#define TERM_APP_HS_UDP_SRV_TX_PORT 13001
+#define TERM_APP_HS_UDP_SRV_RX_PORT 13002
+
+
 
 #define TERM_APP_OK 	0x00
 #define TERM_APP_ERROR	0xFF
@@ -39,6 +43,26 @@ typedef struct term_app_control_msg_{
 } term_app_control_msg_t;
 
 
+
+static int term_udp_send(int sock, void *msg, int size)
+{
+	int res;
+	struct sockaddr_in tx_addr = {0};
+
+	tx_addr.sin_family = AF_INET; 
+    	tx_addr.sin_addr.s_addr = INADDR_ANY;
+	tx_addr.sin_port = htons(TERM_APP_HS_UDP_SRV_RX_PORT);
+
+	res = sendto(listensock, 
+			msg, 
+			size,
+			 MSG_CONFIRM,
+			(const struct sockaddr *) &tx_addr, 
+			(socklen_t) sizeof(tx_addr));
+
+	return res;
+}
+
 void process_term_app_msg(term_app_control_msg_t* termmsg)
 {
 	int bytes;
@@ -46,9 +70,9 @@ void process_term_app_msg(term_app_control_msg_t* termmsg)
 	switch (termmsg->opcode)
 	{
 	case TERM_APP_SERVICE_DISABLE:
-			termmsg->opcode = TERM_APP_OK;
-			service_disabled=1;
-			bytes = write(termsock, termmsg, sizeof(*termmsg));
+			termmsg->opcode  = TERM_APP_OK;
+			service_disabled = 1;
+			bytes = term_udp_send(listensock, termmsg, sizeof(*termmsg));
 			if (bytes < 0){
 				hslog_error("Service disabled Failed to tx:");
 			}
@@ -58,7 +82,7 @@ void process_term_app_msg(term_app_control_msg_t* termmsg)
 	case   TERM_APP_SERVICE_ENABLE:
 			termmsg->opcode = TERM_APP_OK;
 			service_disabled=0;
-			bytes = write(termsock, termmsg, sizeof(*termmsg));
+			bytes = term_udp_send(listensock, termmsg, sizeof(*termmsg));
 			if (bytes < 0){
 				hslog_error("Service enabled Failed to tx:");
 			}
@@ -67,14 +91,14 @@ void process_term_app_msg(term_app_control_msg_t* termmsg)
 
 	case   TERM_APP_SERVICE_STATS:
 			get_sensor_stats(&termmsg->u.stats);
-			bytes = write(termsock, termmsg, sizeof(*termmsg));
+			bytes = term_udp_send(listensock, termmsg, sizeof(*termmsg));
 			if (bytes < 0){
 				hslog_error("Service stats failed to tx:");
 			}
 		break;
 
 	default:
-		hslog_error("Unknown msg %d\n",termmsg->opcode);
+		hslog_error("Unknown msg %d\n", termmsg->opcode);
 	}
 }
 
@@ -84,63 +108,36 @@ void* service_control(void * dummy)
 	    int rc,rval;
 	    int nfds = 1;
 	    int nr_msgs = 0;
-	    struct pollfd fds[2];
+	    struct pollfd fds[1];
 	    term_app_control_msg_t termmsg;
 
 	    for  (;;) {
+		nfds = 1;
+		fds[0].fd = listensock;
+		fds[0].events = POLLIN;
+		fds[0].revents = 0;
 
-	    	nfds = 1;
-			fds[0].fd = listensock;
-			fds[0].events =  POLLIN;
-			fds[0].revents = 0;
-			if (termsock > 0) {
-				fds[1].fd = termsock;
-				fds[1].events = POLLIN;
-				fds[1].revents = 0;
-				nfds = 2;
-			}
+		rc = poll(fds, nfds, -1);
+		if (rc == 0){
+			// timeout.
+			printf("%s %d\n",__func__,__LINE__);
+			continue;
+		}
+		printf("%s %d %x %d\n",__func__,__LINE__,fds[0].revents,rc);
+		if (rc < 0){
+			hslog_error("Polling error %s\n",strerror(errno));
+			continue;
+		}
 
-			int timeout = 5000;
-			rc = poll(fds, nfds, timeout);
-			if (rc == 0){
-				// timeout.
+		if (fds[0].revents & POLLIN) {
+			bzero(&termmsg, sizeof(termmsg));
+			rval = read(listensock, &termmsg, sizeof(termmsg));
+			if (rval <= 0) {
+				hslog_error("reading stream message");
 				continue;
 			}
-
-			if (rc < 0){
-				hslog_error("Polling error %s\n",strerror(errno));
-				continue;
-			}
-
-			if (fds[0].revents & POLLIN) {
-
-				termsock = accept(listensock, 0, 0);
-				if (termsock == -1){
-					hslog_error("accept");
-				}
-				continue;
-			}
-
-			if (fds[1].revents & (POLLHUP|POLLERR)){
-				close(termsock);
-				termsock = -1;
-				continue;
-			}
-
-			if (fds[1].revents & POLLIN) {
-
-				bzero(&termmsg, sizeof(termmsg));
-				rval = read(termsock, &termmsg, sizeof(termmsg));
-				if (rval <= 0) {
-					hslog_error("reading stream message");
-					close(termsock);
-					termsock=-1;
-					continue;
-				}
-
-				process_term_app_msg(&termmsg);
-
-			}
+			process_term_app_msg(&termmsg);	
+		}
 	    }
 	    close(listensock);
 }
@@ -150,24 +147,24 @@ void* service_control(void * dummy)
 
 int start_service_check(char *sockname)
 {
-    struct sockaddr_un server;
+    struct sockaddr_in     addr;
     pthread_t tid;
 
-    unlink(sockname);
     hslog_error("Starting term app control service\n");
-    listensock = socket(AF_UNIX, SOCK_STREAM, 0);
+    listensock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (listensock < 0) {
         hslog_error("opening stream socket");
         return -1;
     }
 
-    server.sun_family = AF_UNIX;
-    strcpy(server.sun_path, sockname);
+   addr.sin_family = AF_INET;
+   addr.sin_addr.s_addr = INADDR_ANY; 
+   addr.sin_port = htons(TERM_APP_HS_UDP_SRV_TX_PORT);
 
-    if (bind(listensock, (struct sockaddr *) &server, sizeof(struct sockaddr_un))) {
+   if (bind(listensock, (struct sockaddr *) &addr, sizeof(addr))) {
         hslog_error("failed on binding stream socket");
         return -1;
-    }
+   }
 
     listen(listensock, 2);
    
